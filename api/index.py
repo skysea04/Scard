e0bbe9bbe37bcacf8d1ac322677669d715909e33
skysea04 <73434165+skysea04@users.noreply.github.com>
2021-06-20 03:56:01 +0800

    完成基礎render文章列表功能，接續render尚未完成（假資料建置中）

--- api/index.py
+++ api/index.py
@@ -9,10 +9,60 @@ def get_posts():
     is_popular = request.args.get('popular')
     select_board = request.args.get('board')
     # 熱門render之後補
-    if is_popular:
+    if is_popular == 'true':
         return
     else:
-        page = request.args.get('page')
+        page = int(request.args.get('page'))
+        next_page = page + 1
+        render_num = 30
+        render_index = page * render_num
+        # 在首頁render的情況
+        if select_board == 'all':
+            posts = db.session.execute('SELECT post.id, post.user_name, post.title, post.content, post.first_img, post.like_count, post.comment_count, postboard.sys_name, postboard.show_name, user.comment_avatar \
+                FROM ((post INNER JOIN postboard ON post.board_id = postboard.id)\
+                INNER JOIN user ON post.user_id = user.id)\
+                ORDER BY id DESC\
+                LIMIT :index, :render_num', {'index': render_index, 'render_num': render_num})
+            next_post = db.session.execute('SELECT id FROM post\
+                ORDER BY id DESC\
+                LIMIT :index, :render_num', {'index': render_index + render_num, 'render_num': 1}).first()
+        # 在各看板render的情況
+        else:
+            posts = db.session.execute('SELECT post.id, post.user_name, post.title, post.content, post.first_img, post.like_count, post.comment_count, postboard.sys_name, postboard.show_name, user.comment_avatar \
+                FROM ((post INNER JOIN postboard ON post.board_id = postboard.id)\
+                INNER JOIN user ON post.user_id = user.id)\
+                WHERE postboard.sys_name = :board\
+                ORDER BY post.id DESC\
+                LIMIT :index, :render_num', {'board': select_board, 'index': render_index, 'render_num': render_num})
+            next_post = db.session.execute('SELECT post.id\
+                FROM post INNER JOIN postboard ON post.board_id = postboard.id\
+                WHERE postboard.sys_name = :board\
+                ORDER BY post.id DESC\
+                LIMIT :index, :render_num', {'board': select_board, 'index': render_index + render_num, 'render_num': 1}).first()
+
+        if not next_post:
+            next_page = None
         
+        post_list = []
+        for post in posts:
+            post = post._asdict()
+            # print(post)
+            post_data = {
+                "url": f"/b/{post['sys_name']}/p/{post['id']}",
+                "avatar": post['comment_avatar'],
+                "board": post['show_name'],
+                "userName": post['user_name'],
+                "title": post['title'],
+                "content": post['content'],
+                "img": post['first_img'],
+                "likeCount": post['like_count'],
+                "commentCount": post['comment_count']
+            }
+            post_list.append(post_data)
         
-    return 
+        data = {
+            "data": post_list,
+            "nextPage": next_page
+        }
+        return jsonify(data), 200
+        

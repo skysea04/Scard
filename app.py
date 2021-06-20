d28490c346d40029bfd2be0c9b0a51e76b4f04ba
skysea04 <73434165+skysea04@users.noreply.github.com>
2021-06-20 18:48:03 +0800

    修正不同權限使用者點選navbar各頁面icon的回應

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	socketio.run(app, host='0.0.0.0', port=8000)
+	socketio.run(app, host='0.0.0.0', port=8000, debug=True)

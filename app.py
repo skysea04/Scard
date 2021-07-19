e56b875184a957470ee0df913aaa1025227afd13
skysea04 <73434165+skysea04@users.noreply.github.com>
2021-07-19 13:03:08 +0800

    修正message路由設定

--- app.py
+++ app.py
@@ -170,6 +170,7 @@ def redirect_message():
 			if message_id:
 				message_id = message_id[0]
 				return redirect(url_for('message', id=message_id))
+		return redirect(url_for('show_board'))
 	except:
 		abort(500)
 
